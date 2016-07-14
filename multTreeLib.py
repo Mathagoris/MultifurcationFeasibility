ec787ae63c5af78cc5ce94d4ed2ffc14e7d4603a
mathagoris <matt.dohlen@gmail.com>
2016-07-14 12:01:31 -0700

    Almost done with feasibility test

--- multTreeLib.py
+++ multTreeLib.py
@@ -29,10 +29,41 @@ class Tree:
         Phylo.draw_ascii(self.tree)
 
     def is_feasible(self):
+        for cc in nx.connected_components(self.LEG):
+            for i in xrange(0, len(cc)):
+                for j in xrange(i+1, len(cc)):
+                    if cc[i].split('_')[0] == cc[j].split('_')[0]:
+                        return False
         return True
 
     def generate_LEG(self):
-        return nx.Graph()
+        LEG = nx.Graph()
+        group_paths = {}
+        for group in self.group_terminals():
+            group_locus = self.loci(group[0])
+            for terminal in group[1:]:
+                path = self.tree.trace(group[0], terminal)
+                if group_locus in group_paths:
+                    group_paths[group_locus].append(set(path))
+                else:
+                    group_paths[group_locus] = [set(path)]
+        group_loci = group_paths.keys()
+        for i in xrange(0, len(group_loci)):
+            loci_1 = group_loci[i]
+            for j in xrange(i+1, len(group_loci)):
+                loci_2 = group_loci[j]
+                if not self.paths_are_edge_disjoint(group_paths[loci_1], group_paths[loci_2]):
+                    LEG.add_edge(loci_1, loci_2)
+        return LEG
+
+    def group_terminals(self):
+
+    def paths_are_edge_disjoint(self, paths_1, paths_2):
+        for path_1 in paths_1:
+            for path_2 in paths_2:
+                if len(path_1.instersect(path_2)) > 2:
+                    return False
+        return True
 
     def get_terminals_w_path(self, from_list):
         to_list = list(set(self.tree.get_terminals) - set(from_list))
@@ -97,8 +128,8 @@ class Tree:
                     else:
                         partition['no_path'] = [child]
                 else:
-                    # Arbitrarily choose the first loci on the parent edge because all the paths
-                    # are in the same connected component regardless
+                    # Arbitrarily choose the first loci on the parent edge because all the loci with
+                    # paths on parent edge are in the same connected component regardless
                     cc = tuple(nx.node_connected_component(self.LEG, self.loci(paths_on_parent_edge[0])))
                     if cc in partition:
                         partition[cc].append(child)
